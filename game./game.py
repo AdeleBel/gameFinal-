38d8dbb95f8e8f83f3847377a30db8aa8912df0c
AdeleBel <adele.robison@ucas-edu.net>
2024-10-25 19:43:48 +0000

    gyatt stated
    Co-authored-by: coolguyben <coolguyben@users.noreply.github.com>
    Co-authored-by: AmandaAnguiano <AmandaAnguiano@users.noreply.github.com>

---
+++ game./game.py
@@ -0,0 +1,20 @@
+
+
+# asking names the player wants to use for themselves and characters and intoduce story, keep track of players alive state - ben 
+user = input("Enter Your name: ")
+print("Hello,", user, "Welcome to The Path! You will have to make the correct choices to beat the game.(If you mispell anything it will crash)")
+joe = input("Enter the name for your friend: ")
+jim = input("One more name: ")
+isJoeAlive = True
+isJimAlive = True
+isGameOver = False
+print("You,",joe,"and",jim, "are going camping, you are on your way to the cabin. It starts to storm, you hide in a cave but get lost! The storm has passed now. Try and get out.")
+#asking what the options/directions you want to go (forest, mountain, river, graveyard, car) and the outcome-adele 
+
+#keep collecting berrys in the forest until you have over 25(while loop) -amanda
+
+#if player says to continue playing, then go into the car to go to the seance to bring back friends at the graveyard-adele
+
+
+
+
